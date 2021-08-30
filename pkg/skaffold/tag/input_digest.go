75eefaa86227c222e7de09afab61387fe339a11f
Micke Löwenadler <mikaell@spotify.com>
2021-08-30 13:31:18 +0200

    Try relative path first and use absolute path as fallback

--- pkg/skaffold/tag/input_digest.go
+++ pkg/skaffold/tag/input_digest.go
@@ -95,9 +95,10 @@ func fileHasher(path string, workspacePath string) (string, error) {
 	// This will ensure we will always get the same hash independent of workspace location and hierarchy.
 	relPath, err := filepath.Rel(workspacePath, path)
 	if err != nil {
-		return "", err
+		h.Write([]byte(filepath.Clean(path)))
+	} else {
+		h.Write([]byte(relPath))
 	}
-	h.Write([]byte(relPath))
 	if fi.Mode().IsRegular() {
 		f, err := os.Open(path)
 		if err != nil {
